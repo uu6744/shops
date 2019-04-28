e1bd548cf400ee5cf029b008644be411a31fd0d5
zhengyaru  <1715574790@qq.com>
2019-04-28 16:03:22 +0800

    'zyr'

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::post('/test', 'TestController@edit');
 Route::get('/excel', 'CeshiController@export');
