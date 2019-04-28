228162ce7341cdedb3be94aea819fa2d25508ebb
zhengyaru  <1715574790@qq.com>
2019-04-28 15:16:49 +0800

    'zyr'

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -231,6 +233,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+
+       'Form' => Collective\Html\FormFacade::class,
+      'Html' => Collective\Html\HtmlFacade::class,
         //
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
